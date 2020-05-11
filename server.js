23401a777d594fa2350b3e9cee51b85b45c40cf5
Ravalash <13k0873juhky@opayq.net>
2020-05-10 22:12:38 -0400

    Adding error trapping for file reading and writing

--- server.js
+++ server.js
@@ -30,23 +30,42 @@ app.get("/notes", function (req, res) {
 // Route that returns db.json object when callled
 app.get("/api/notes", function (req, res) {
   res.json(
-    JSON.parse(fs.readFileSync(path.join(__dirname, "./db/db.json"), "utf8"))
+    JSON.parse(
+      fs.readFileSync(path.join(__dirname, "./db/db.json"), "utf8", function (
+        error
+      ) {
+        if (error) {
+          return console.log(error);
+        }
+      })
+    )
   );
 });
 
 // Route to create a new note - takes in JSON input, adds to db.json, returns note to client
 app.post("/api/notes", function (req, res) {
   const existingNotes = JSON.parse(
-    fs.readFileSync(path.join(__dirname, "./db/db.json"), "utf8")
+    fs.readFileSync(path.join(__dirname, "./db/db.json"), "utf8", function (
+      error
+    ) {
+      if (error) {
+        return console.log(error);
+      }
+    })
   );
   existingNotes.unshift(req.body);
   // Adds an ID value to each note sequentially
   existingNotes.forEach((element, index) => {
-    element.id = index+1;
+    element.id = index + 1;
   });
   fs.writeFileSync(
     path.join(__dirname, "./db/db.json"),
-    JSON.stringify(existingNotes)
+    JSON.stringify(existingNotes),
+    function (error) {
+      if (error) {
+        return console.log(error);
+      }
+    }
   );
   res.json(existingNotes);
 });
@@ -54,7 +73,13 @@ app.post("/api/notes", function (req, res) {
 // Deletes a note by ID passed
 app.delete("/api/notes/:id", function (req, res) {
   const existingNotes = JSON.parse(
-    fs.readFileSync(path.join(__dirname, "./db/db.json"), "utf8")
+    fs.readFileSync(path.join(__dirname, "./db/db.json"), "utf8", function (
+      error
+    ) {
+      if (error) {
+        return console.log(error);
+      }
+    })
   );
   // Filters the note matching the ID passed from the array
   const updatedNotes = existingNotes.filter(function (note) {
@@ -62,7 +87,12 @@ app.delete("/api/notes/:id", function (req, res) {
   });
   fs.writeFileSync(
     path.join(__dirname, "./db/db.json"),
-    JSON.stringify(updatedNotes)
+    JSON.stringify(updatedNotes),
+    function (error) {
+      if (error) {
+        return console.log(error);
+      }
+    }
   );
   res.json(updatedNotes);
 });
